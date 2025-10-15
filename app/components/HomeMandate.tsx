f2765dbc6046061455a8812af4ced9295dcf9638
dynamicspaceconsulting <dynamicspaceconsulting@gmail.com>
2025-10-15 16:50:37 +0100

    Update HomeMandate.tsx

--- app/components/HomeMandate.tsx
+++ app/components/HomeMandate.tsx
@@ -18,8 +18,10 @@ export default function HomeMandate() {
       <section className="flex flex-col md:flex-row items-center justify-between md:space-x-20 gap-10  md:my-16 p-8 md:p-0">
         {/* Text Section */}
         <div className="w-full md:w-1/2 space-y-2">
-          <p className=" font-normal text-base md:text-lg text-[#1A202C]">
-            Dynamic Space Consulting (DSC) is a global fundraising
+          <p className=" font-normal text-base md:text-lg">
+            <span className="font-semibold text-[#0A3D62]">
+              Dynamic Space Consulting (DSC){" "}
+            </span>{" "} is a global fundraising
             consultancy firm dedicated to empowering non profits,
             startups, and social enterprises with the resources they 
             need to thrive.
